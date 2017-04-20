21351ef4dfeb3f6a23824e852f27207cc3100eb5
SarahPAms <Sarah.L.Polan@gmail.com>
2017-04-20 15:59:52 +0200

    added toggle for admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,12 +2,17 @@ class AdminsController < ApplicationController
 
   def index
     @matches = Match.all
+    @users = User.all
   end
 
   def show
+    user = User.find(params[:id])
+    user.admin = !user.admin
+    user.save
+    redirect_to admins_path, notice: "User was updated"
   end
 
   def update
+    @dates = [3.days.from_now, 4.days.from_now, 6.days_from_now, 7.days_from_now, 8.days_from_now]
   end
-
 end
