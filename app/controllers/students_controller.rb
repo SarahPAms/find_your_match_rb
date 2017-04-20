9c17fe220dd69fae8cb0d98cbdad31c7418c853c
SarahPAms <Sarah.L.Polan@gmail.com>
2017-04-20 20:58:15 +0200

    added automatic assigning

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,37 @@
 class StudentsController < ApplicationController
   before_action :set_current_user, only: [:show, :new, :create]
   def index
+
     @matches= Match.all
-    #where current user is user and date is < today
-    @match = Match.where(date: Date.today)
+    @match = Match.where(date: Date.today, user: current_user.id)
+
+    if true
+      @users = User.where.not("id =?", current_user.id)
+      @old_matches = Match.where("user_id =? AND date < ?", current_user.id, Date.today)
+
+      @user_ids = []
+      @users.each do |user|
+        @user_ids.push(user.id)
+      end
+
+      @match_names = []
+      @old_matches.each do | match |
+        user = User.where(name: match.matched_user)
+        @match_names.push(user.id)
+      end
+
+      @final_draw = @user_ids - @match_names
+      @random_id = @final_draw.sample
+      selected_user = User.where("id =?", @random_id).pluck(:name)
+
+
+      inserted_match=Match.create(date: Date.today, user_id: current_user.id, matched_user: selected_user[0])
+
+
+    else
+      @monkey ="tree"
+
+    end
   end
 
   def show
